1fb914e2b89499430f59f34699e915fef8423857
Daio-io <dai.williams@bbc.co.uk>
2017-01-08 19:15:45 +0000

    fudge image size for now

--- api/lib/search.js
+++ api/lib/search.js
@@ -24,10 +24,13 @@ function parseResult(result) {
     let json = JSON.parse(result.body);
 
     json.results = json.results.map((item) => {
+
+console.log(item)
+
       return {
         artist: item.artistName,
         track: item.trackName,
-        imageUrl: item.artworkUrl100,
+        imageUrl: item.artworkUrl100.replace('100x100', '500x500'),
         feedUrl: item.feedUrl,
         collection: item.collectionName
       }
