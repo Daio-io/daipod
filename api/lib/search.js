bd1aecd6290d0a29e253a20bea819ba84dce7cea
Daio-io <dai.williams@bbc.co.uk>
2017-01-12 00:08:24 +0000

    update image size

--- api/lib/search.js
+++ api/lib/search.js
@@ -30,7 +30,7 @@ console.log(item)
       return {
         artist: item.artistName,
         track: item.trackName,
-        imageUrl: item.artworkUrl100.replace('100x100', '300x300'),
+        imageUrl: item.artworkUrl100.replace('100x100', '500x500'),
         feedUrl: item.feedUrl,
         collection: item.collectionName
       }
